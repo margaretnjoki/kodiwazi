27aab976dbe433df78cce6798826b9d71200a59d
margaretnjoki <maggykiroko@gmail.com>
2026-08-05 14:54:38 +0300

    add annotations to region

--- src/main/java/com/margaretnjoki/kodiwazi/entity/Region.java
+++ src/main/java/com/margaretnjoki/kodiwazi/entity/Region.java
@@ -1,11 +1,20 @@
 package com.margaretnjoki.kodiwazi.entity;
 
+import jakarta.persistence.*;
+
 import java.time.Instant;
 import java.util.UUID;
 
+@Entity
+@Table(name = "regions")
 public class Region {
+    @Id
+    @GeneratedValue
     private UUID id;
+    @Column(nullable = false, unique = true)
     private String name;
+    @Column(nullable = false)
     private Instant createdAt;
+    @Column(nullable = false)
     private Instant updatedAt;
 }
