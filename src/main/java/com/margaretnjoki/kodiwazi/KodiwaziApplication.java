8a9c794fa606e00bec35d317f4268aa821c38b46
margaretnjoki <maggykiroko@gmail.com>
2026-08-05 11:16:46 +0300

    initial

---
+++ src/main/java/com/margaretnjoki/kodiwazi/KodiwaziApplication.java
@@ -0,0 +1,13 @@
+package com.margaretnjoki.kodiwazi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodiwaziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodiwaziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/margaretnjoki/kodiwazi/KodiwaziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.margaretnjoki.kodiwazi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodiwaziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
