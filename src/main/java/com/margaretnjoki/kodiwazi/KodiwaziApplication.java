46f44c6d0927ea0a2bc8acf4ee6dda5db171d8d5
margaretnjoki <maggykiroko@gmail.com>
2026-08-14 07:10:14 +0300

    create the database and first v1__init.sql migration to create tables

--- src/main/java/com/margaretnjoki/kodiwazi/KodiwaziApplication.java
+++ src/main/java/com/margaretnjoki/kodiwazi/KodiwaziApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KodiwaziApplication {
+public class  KodiwaziApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KodiwaziApplication.class, args);
