a65cb9cb1f623f92844c57226c13ea4245147d2d
margaretnjoki <maggykiroko@gmail.com>
2026-08-16 18:44:57 +0300

    add method to find Contributor by email in ContributorRepository

--- src/main/java/com/margaretnjoki/kodiwazi/repository/ContributorRepository.java
+++ src/main/java/com/margaretnjoki/kodiwazi/repository/ContributorRepository.java
@@ -3,9 +3,13 @@ package com.margaretnjoki.kodiwazi.repository;
 import com.margaretnjoki.kodiwazi.entity.Contributor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ContributorRepository extends JpaRepository<Contributor, UUID> {
 
     boolean existsByEmail(String email);
+
+    Optional<Contributor> findByEmail(String email);
 }
+
