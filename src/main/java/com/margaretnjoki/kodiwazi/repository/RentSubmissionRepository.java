98d9dba681b2d551b173a40cc6ca675fd9709584
margaretnjoki <maggykiroko@gmail.com>
2026-08-22 09:43:40 +0300

    fix(repository): update RentSubmissionRepository to use RentSubmission entity

--- src/main/java/com/margaretnjoki/kodiwazi/repository/RentSubmissionRepository.java
+++ src/main/java/com/margaretnjoki/kodiwazi/repository/RentSubmissionRepository.java
@@ -1,9 +1,10 @@
 package com.margaretnjoki.kodiwazi.repository;
 
 import com.margaretnjoki.kodiwazi.entity.Region;
+import com.margaretnjoki.kodiwazi.entity.RentSubmission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface RentSubmissionRepository extends JpaRepository<Region, UUID> {
+public interface RentSubmissionRepository extends JpaRepository<RentSubmission, UUID> {
 }
