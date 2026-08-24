3b13dfa0fba64e6d66de244f0fd38ce32477a556
margaretnjoki <maggykiroko@gmail.com>
2026-08-24 07:21:26 +0300

    feat(rent): update RentSubmissionController to use Authentication for contributor email

--- src/main/java/com/margaretnjoki/kodiwazi/controller/RentSubmissionController.java
+++ src/main/java/com/margaretnjoki/kodiwazi/controller/RentSubmissionController.java
@@ -3,6 +3,7 @@ package com.margaretnjoki.kodiwazi.controller;
 import com.margaretnjoki.kodiwazi.dtos.RentSubmissionRequest;
 import com.margaretnjoki.kodiwazi.dtos.RentSubmissionResponse;
 import com.margaretnjoki.kodiwazi.service.RentSubmissionService;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,20 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/rent_submissons")
+@RequestMapping("/rent-submissions")
 public class RentSubmissionController {
-public  final RentSubmissionService rentSubmissionService;
+    public final RentSubmissionService rentSubmissionService;
 
     public RentSubmissionController(RentSubmissionService rentSubmissionService) {
         this.rentSubmissionService = rentSubmissionService;
     }
 
     @PostMapping
-    public RentSubmissionResponse submit(@RequestBody RentSubmissionRequest request, String contributorEmail) {
-        return rentSubmissionService.submit(request,contributorEmail);
+    public RentSubmissionResponse submit(@RequestBody RentSubmissionRequest request, Authentication authentication) {
+        String contributorEmail = authentication.getName();
+        return rentSubmissionService.submit(request, contributorEmail);
     }
-
-
 
 
 }
