53a9f9604aafe4a287ca806f24f7ff8fead69069
margaretnjoki <maggykiroko@gmail.com>
2026-08-24 06:40:16 +0300

    feat(rent): implement RentSubmissionController with submission endpoint

--- src/main/java/com/margaretnjoki/kodiwazi/controller/RentSubmissionController.java
+++ src/main/java/com/margaretnjoki/kodiwazi/controller/RentSubmissionController.java
@@ -1,5 +1,29 @@
 package com.margaretnjoki.kodiwazi.controller;
 
+import com.margaretnjoki.kodiwazi.dtos.RentSubmissionRequest;
+import com.margaretnjoki.kodiwazi.dtos.RentSubmissionResponse;
+import com.margaretnjoki.kodiwazi.service.RentSubmissionService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/rent_submissons")
 public class RentSubmissionController {
+public  final RentSubmissionService rentSubmissionService;
+
+    public RentSubmissionController(RentSubmissionService rentSubmissionService) {
+        this.rentSubmissionService = rentSubmissionService;
+    }
+
+    @PostMapping
+    public RentSubmissionResponse submit(@RequestBody RentSubmissionRequest request, String contributorEmail) {
+        return rentSubmissionService.submit(request,contributorEmail);
+    }
+
+
+
 
 }
