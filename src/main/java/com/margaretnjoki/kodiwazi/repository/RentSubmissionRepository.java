dfb53c4a9acb2eac4dcf9484827424820731fd40
margaretnjoki <maggykiroko@gmail.com>
2026-08-24 14:27:30 +0300

    feat(repository): add method to find rent submissions by area, house type, and status

--- src/main/java/com/margaretnjoki/kodiwazi/repository/RentSubmissionRepository.java
+++ src/main/java/com/margaretnjoki/kodiwazi/repository/RentSubmissionRepository.java
@@ -1,10 +1,17 @@
 package com.margaretnjoki.kodiwazi.repository;
 
+import com.margaretnjoki.kodiwazi.entity.HouseType;
 import com.margaretnjoki.kodiwazi.entity.RentSubmission;
+import com.margaretnjoki.kodiwazi.entity.SubmissionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RentSubmissionRepository extends JpaRepository<RentSubmission, UUID> {
-
+    List<RentSubmission> findByAreaIdAndHouseTypeAndStatus(
+            UUID areaId,
+            HouseType houseType,
+            SubmissionStatus status
+    );
 }
