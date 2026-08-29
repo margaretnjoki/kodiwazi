4e000efeb1e182b57a88a16c55b80499e63dc539
margaretnjoki <maggykiroko@gmail.com>
2026-08-29 07:11:32 +0300

    feat(repository): add methods to find areas by region ID and name

--- src/main/java/com/margaretnjoki/kodiwazi/repository/AreaRepository.java
+++ src/main/java/com/margaretnjoki/kodiwazi/repository/AreaRepository.java
@@ -4,7 +4,13 @@ import com.margaretnjoki.kodiwazi.entity.Area;
 import com.margaretnjoki.kodiwazi.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AreaRepository extends JpaRepository<Area, UUID> {
+    List<Area> findByRegionId(UUID regionId);
+
+    List<Area> findByNameContainingIgnoreCase(String name);
+
+    List<Area> findByRegionIdAndNameContainingIgnoreCase(UUID regionId, String name);
 }
