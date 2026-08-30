ad244fdbec4fff08f9b20970406ba8d429baa736
margaretnjoki <maggykiroko@gmail.com>
2026-08-30 08:03:52 +0300

    fix(dtos): add validation constraints for email and password in LoginRequest

--- src/main/java/com/margaretnjoki/kodiwazi/dtos/LoginRequest.java
+++ src/main/java/com/margaretnjoki/kodiwazi/dtos/LoginRequest.java
@@ -1,7 +1,13 @@
 package com.margaretnjoki.kodiwazi.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequest(
+
+        @NotBlank(message = "email is required")
         String email,
+
+        @NotBlank(message = "password is required")
         String password
 ) {
 }
